e99ea773bbe4e5ce33641d7c60b9911d6b68b3ea
Mateo Clarke <code@mateoclarke.com>
2017-03-18 18:30:01 -0500

    wip: Trying to get sub Routes to work inside TeacherExp

--- legit-o-meter/src/components/TeacherDashboard.js
+++ legit-o-meter/src/components/TeacherDashboard.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import GameListItem from './GameListItem';
 import Header from './Header';
-import { Link } from 'react-router-dom';
+import NewGame from './NewGame';
+import { Link, Route, Switch } from 'react-router-dom';
 import { userData, collections } from '../seedData'
+
+const TeacherHome = ({ match }) => {
+  debugger
+  return (
+    <div>
+      <h1>Teacher Dashboard</h1>
+      <Link to={`${match.url}new`}>New Game</Link>
+      <h3 className="dashboardTitle">Your Games</h3>
+      <table className="gameList">
+        <tbody>
+          {userData.user.games.map((game) => {
+            // TODO - Change collection to get object by ID not index
+            return(
+              <GameListItem
+                name={game.name}
+                collection={collections[game.collectionId]}
+                key={game.id}
+              />
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 class TeacherDashboard extends React.Component {
   constructor() {
@@ -13,27 +39,13 @@ class TeacherDashboard extends React.Component {
   }
 
   render() {
-
     return (
       <div className="dashboard">
         <Header />
-        <h1>Teacher Dashboard</h1>
-        <Link to='/game/new'>New Game</Link>
-        <h3 className="dashboardTitle">Your Games</h3>
-        <table className="gameList">
-          <tbody>
-            {userData.user.games.map((game) => {
-              // TODO - Change collection to get object by ID not index
-              return(
-                <GameListItem
-                  name={game.name}
-                  collection={collections[game.collectionId]}
-                  key={game.id}
-                />
-              )
-            })}
-          </tbody>
-        </table>
+          <Switch>
+            <Route pattern="/game" exact component={TeacherHome}/>
+            <Route pattern="/game/new" component={NewGame}/>
+          </Switch>
       </div>
     )
   }
--- legit-o-meter/src/index.js
+++ legit-o-meter/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import {
   Route,
@@ -9,18 +9,49 @@ import {
 import './index.css';
 import App from './App';
 import NewGame from './components/NewGame';
+import Login from './components/Login'
+import TeacherDashboard from './components/TeacherDashboard';
+import StudentDashboard from './components/StudentDashboard';
 
-const Root = () => {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact pattern="/" component={App} />
-          <Route pattern="/game/new" component={NewGame}/>
-        </Switch>
+
+
+class Root extends Component {
+  constructor() {
+    super();
+    this.state = {
+      teacherLoggedIn: true,
+      studentLoggedIn: false,
+    }
+  }
+
+  toggleExperience() {
+    const teacher = this.state.teacherLoggedIn;
+    const student = this.state.studentLoggedIn;
+    let display = <Login />;
+    if (teacher) {
+      display = <TeacherDashboard />;
+     } else if (student){
+      display = <StudentDashboard />;
+    }
+    return (
+      <div className="App">
+        {display}
       </div>
-    </Router>
-  )
+    );
+  }
+
+  render(){
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact pattern="/" render={this.toggleExperience.bind(this)} />
+            <Route pattern="/game/new" component={NewGame}/>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 render(<Root/>, document.querySelector('#root'));
