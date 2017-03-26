800154709025cfe26ad6d5fa5d99e506221047a9
Mateo Clarke <code@mateoclarke.com>
2017-03-26 15:52:07 -0500

    Update CSS on teacher root dashboard

--- app/src/components/Admin/GameListItem.js
+++ app/src/components/Admin/GameListItem.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 
 class GameListItem extends React.Component {
   render() {
     return (
       <tr key={this.props.id}>
-        <td><Link to={`/game/${this.props.id}`} className="reviewButton">Review</Link></td>
-        <td><h5 className="gameName">{this.props.name} - {this.props.collection} </h5></td>
-        <td><button className="deleteButton">Delete</button></td>
+        <td>
+          <h5 className="gameName">
+            {this.props.name} - {this.props.collection}
+          </h5>
+        </td>
+        <td className="collapsing">
+          <Link to={`/game/${this.props.id}`} className="ui button blue">
+            Review
+          </Link>
+          <Button floated right basic color="red">
+            Delete
+          </Button>
+        </td>
       </tr>
     )
   }
 }
 
 export default GameListItem;
-
-//{this.props.collection.name}{this.props.collection}
