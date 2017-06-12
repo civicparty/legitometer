295bf24c382eb637326278eb7eae06a05d2d09c1
Rally Jinx <rsjinx@gmail.com>
2017-06-12 11:48:38 -0500

    clean up ArticleInput.js

--- app/src/components/Admin/ArticleInput.js
+++ app/src/components/Admin/ArticleInput.js
@@ -9,28 +9,13 @@ const articleTypes = [
 class ArticleInput extends React.Component {
 
   handleChange(e, result) {
-    // console.log("soooo", result.id);
-    // *** this works sometimes but not other times WHYYYYYY? and when it does work it only works for the first input ***
-
-    // this sets the "role" Attribute of the target to option?
-        // is it supposed to reference options in the form?
+  
     const isDropdown = e.target.getAttribute("role") === "option"
-    // i forgot what ? : does exactly and i can't google it...
-    // console.log("line 2, isDropdown", isDropdown, "e.target.closest",  e.target.closest(".dropdown"),"e.target.parent", e.target.parentElement);
 
-    const inputElement = isDropdown ? e.target.closest(".dropdown") : e.target.parentElement
-    // isDropdown is evaluating to false when it shouldn't be and thus it is not selecting the correct thing
-    // console.log("inputElement.id", inputElement.id);
-    const index = result.id.split("_")[2]; // these are undefined for ArticleTypes
+    const index = result.id.split("_")[2];
     const fieldName = result.id.split("_")[1];
-
-    // const index = inputElement.id.split("_")[2]; // these are undefined for ArticleTypes
-    // const fieldName = inputElement.id.split("_")[1];
     const value = isDropdown ? e.target.innerText : result.value ;
-    // const value = isDropdown ? e.target.innerText : e.target.value ;
-    // console.log("ok, i'm saving the things", index, fieldName, value);
-    // so this should send the index where to put the stuff in this.state.articles,
-    // the fieldName and the value... to handleArticleInputChange() in CreateCollection.js
+
     this.props.handleArticleInputChange(index, fieldName, value)
   }
 
