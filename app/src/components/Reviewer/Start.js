897e93b8abc391ffbc4c269e4ffc28d53e1cc525
Sean Dellis <sean.dellis@gmail.com>
2017-06-24 19:16:52 -0500

    convert text to smart quotes

--- app/src/components/Reviewer/Start.js
+++ app/src/components/Reviewer/Start.js
@@ -9,13 +9,13 @@ import legitCatImage from '../../images/legit-cat.png';
 class Start extends Component {
   render() {
     const bodyStyles = { marginBottom: "50px" }
-    
+
     return (
       <div className="text-center">
         <div style={bodyStyles}>
           <img src={legitCatImage} alt="Legit Cat Welcomes You" />
-          <h1>You'll evaluate 3 different articles and figure out which ones are legit.</h1>
-          <p>We'll ask you questins to help you determine the answer.</p>
+          <h1>You’ll evaluate 3 different articles and figure out which ones are legit.</h1>
+          <p>We’ll ask you questins to help you determine the answer. <a href="#"> Testing this Crazy shit out dog</a></p>
         </div>
         <Link to="/article/1">
           <Button text="Let's Get Started" />
