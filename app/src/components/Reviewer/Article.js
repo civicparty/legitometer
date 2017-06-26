15b03a42befd7034b477b4ba31285d1841cf803a
Mateo Clarke <code@mateoclarke.com>
2017-06-25 20:56:35 -0500

    Pull article data from API and display one

--- app/src/components/Reviewer/Article.js
+++ app/src/components/Reviewer/Article.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 import Button from '../Shared/Button';
 
+
 class Article extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headline: '',
+      url: ''
+    };
+  }
+
+  componentWillMount() {
+    console.log("requesting articles");
+    // TODO: This API needs to be refactored. We should write a route that
+    // allows you to grab articles by their casefile id.
+    // EX: "/api/articles/casefile/2"
+    //
+    // for demonstration purposes, shortcutting to grab a specific one in the array.
+    axios.get('http://localhost:8888/api/articles')
+      .then((res) => {
+        const { headline, url } = res.data.data[27].article
+        this.setState({
+          headline: headline,
+          url: url
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
 
   render() {
     const bodyStyles = { marginBottom: '50px' }
-    const articleLink = "http://website.example";
-    const articleName = "Example Article Name";
+    const { headline, url } = this.state;
 
     return (
       <div className="flex-column">
@@ -16,7 +45,7 @@ class Article extends Component {
           <h1>Start by opening the article below</h1>
           <p className="tip">We’ll open it in a new window so you can refer back to it.</p>
 
-          <p><a className="article" href={articleLink} target="_blank">{articleName}</a></p>
+          <p><a className="article" href={url} target="_blank">{headline}</a></p>
 
           <Button text="Done! I’m ready to go" />
 
