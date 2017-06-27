73822bf34c7b8719177cade052332217bb680520
Mateo Clarke <code@mateoclarke.com>
2017-06-27 01:19:23 -0500

    …hoping

--- app/knexfile.js
+++ app/knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      host:     '0.0.0.0',
+      host:     'localhost',
       database: process.env.DATABASE_URL,
       user:     process.env.DB_USER,
       password: process.env.DB_PASS
