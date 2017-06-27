64c067a291950499883cb7b33e843997f498126a
Mateo Clarke <code@mateoclarke.com>
2017-06-26 22:50:38 -0500

    update production connection

--- app/knexfile.js
+++ app/knexfile.js
@@ -13,9 +13,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DATABASE_URL,
     },
     pool: {
       min: 2,
