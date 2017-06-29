a4c1ba63fff29530728c8ea730d7ad96281291cd
Mateo Clarke <code@mateoclarke.com>
2017-06-29 16:30:53 -0500

    clean up

--- app/knexfile.js
+++ app/knexfile.js
@@ -13,7 +13,12 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://tacoplanet:' + process.env.DB_PASS + '@localhost/' + process.env.DATABASE_URL,
+    connection: {
+      host:     'localhost',
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
     pool: {
       min: 2,
       max: 10
