cc14f754a88a9bc27e65cfee5a9a928fa416676c
Mateo Clarke <code@mateoclarke.com>
2017-08-03 01:06:13 -0500

    move all the dirt

--- client/src/Collections/reviews.js
+++
@@ -1,8 +0,0 @@
-const bookshelf = require('../../db/knex');
-const Review = require('../Models/Review');
-
-const Reviews = bookshelf.Collection.extend({
-  model: Review
-});
-
-module.exports = bookshelf.collection(Reviews)
