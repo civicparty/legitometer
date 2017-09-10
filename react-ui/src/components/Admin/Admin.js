97da67c182c57199dd23b5f350bf948f8869108d
Mateo Clarke <code@mateoclarke.com>
2017-09-10 17:12:34 -0500

    Give admin pages the `/admin/…` namespace

---
+++ react-ui/src/components/Admin/Admin.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+import NewGame from './NewGame';
+import Dashboard from './Dashboard';
+import Mission from './Mission';
+import CreateCollection from './CreateCollection';
+
+class Admin extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <p>welcome Teacher</p>
+          <Route exact path="/admin" component={Dashboard} />
+          <Route path="/admin/new" component={NewGame} />
+          <Route path="/admin/mission/:id" component={Mission} />
+          <Route path="/admin/collection/new" component={CreateCollection} />
+        </div>
+      </Router>
+    );
+  }
+
+}
+
+export default Admin;
--- react-ui/src/components/Admin/MissionListItem.js
+++ react-ui/src/components/Admin/MissionListItem.js
@@ -30,7 +30,7 @@ class MissionListItem extends React.Component {
         <td className="strong">{this.props.name}</td>
         <td>{this.props.collection}</td>
         <td className="collapsing">
-          <Link to={`/mission/${this.props.id}`} className="ui button blue">
+          <Link to={`/admin/mission/${this.props.id}`} className="ui button blue">
             Review
           </Link>
           <Button type="button" onClick={this.handleDelete} basic color="red">
