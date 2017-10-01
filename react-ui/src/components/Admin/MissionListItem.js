0f5391c07c9f400f5d15d3e5c4813859ddfca641
Rally Jinx <rsjinx@gmail.com>
2017-10-01 10:41:41 -0500

    fix viewCasefile showing up in createCollection because of similar path names

--- react-ui/src/components/Admin/MissionListItem.js
+++ react-ui/src/components/Admin/MissionListItem.js
@@ -36,7 +36,7 @@ class MissionListItem extends React.Component {
       <tr key={missionId}>
         <td className="strong">{missionName}</td>
         <td>
-          <Link to={`/admin/collection/${casefileId}`}>
+          <Link to={`/admin/casefile/${casefileId}`}>
             {casefileName}
           </Link>
         </td>
