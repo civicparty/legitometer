47bced63b1f1e97c08109b54bafa52641e7b4da9
Mateo Clarke <code@mateoclarke.com>
2017-10-01 21:12:05 -0500

    Updates to Start page

--- react-ui/src/components/Reviewer/Start.js
+++ react-ui/src/components/Reviewer/Start.js
@@ -7,27 +7,17 @@ import legitCatImage from '../../images/legit-cat.png';
 class Start extends Component {
 
   render() {
-    const bodyStyles = { marginBottom: '50px' }
-    const headerStyles = { position: 'absolute', top: '120px', left: '60%' }
-    const divStyles = {
-      position: 'relative',
-      flex: 1,
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-    }
-
     return (
-      <div className="text-center" style={divStyles}>
-        <div style={bodyStyles}>
-          <h1 style={headerStyles}>Legit-o-Meter</h1>
+      <div className="ReviewerStart">
+        <div className="ReviewerStart__body">
+          <h1 className="ReviewerStart__header header">Legit or Not</h1>
           <img src={legitCatImage} alt="Legit Cat Welcomes You" />
-          <h1>You’ll read 3 different articles and figure out which ones are legit.</h1>
+          <h2 className="subheader">You’ll read 3 different articles and figure out which ones are legit.</h2>
           <p className="tip">We'll ask you questions to help you determine the answer.</p>
-        </div>        <Link to="/article/0">
+        </div>
+        <Link to={`/article/${this.props.match.params.id}`}>
           <Button text="Let’s Get Started" />
         </Link>
-
       </div>
 
     );
