34bcd6259985684afcb4b3bbc6f67e5212a8f9cc
Rally Jinx <rsjinx@gmail.com>
2017-10-16 13:21:45 -0500

    start on saving reviews and responses - WIP

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -33,15 +33,15 @@ router.get('/api/reviews/:id', function(req, res, next) {
 
 router.post('/api/add-review', function(req, res, next) {
   console.log("made it to the review post route, here's the stuff: ", req.body);
-  // each time 'next' is clicked, stuff from that page will be saved
-  //THIS WILL NOT WORK TO SUBMIT THEM ONE BY ONE
-  // group_id, mission_id, {question: answer}
+  // on initial submit, save group_id and mission_id to reviews table, return review_id
+  // TODO how to tell it is initial submit?
   Review.forge({
     group_id: req.body.groupid,
     mission_id: req.body.missionid,
   })
   .save()
   .then((review) => {
+    res.send(review.id);
     res.sendStatus(200);
   })
   .catch((err) => {
@@ -55,24 +55,3 @@ module.exports = router;
 // reviews table
 // table.integer('user_id').references('users.id')
 // table.integer('mission_id').references('missions.id')
-// table.json('answers');
-
-// {id: 1, user_id: 2, mission_id: 1,
-//   answers: {
-//     publisher_name: 'Sprocket', // references to questions rather than "publisher_name, etc"
-//     author: 'your mom',
-//     headline: 'I like fish',
-//     published_date: '01/03/1983',
-//     summary: 'Fish are tasty',
-//     sources: 'My mouth',
-//     ads: 'minimal',
-//     objectivity: 'none',
-//     page_design: 'no',
-//     spelling: 'lots',
-//     article_type: 'opinion',
-//     credibility_rating: 3,
-//     reputablity_rating: 4,
-//     citation_rating: 2,
-//     headline_rating: 5
-//     }
-//   },
