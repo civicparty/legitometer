76a7f2dfbeaa412e677e65175de857d3c59e4e52
Rally Jinx <rsjinx@gmail.com>
2017-10-30 21:34:18 -0500

    complete page displays

--- react-ui/src/components/Reviewer/Complete.js
+++ react-ui/src/components/Reviewer/Complete.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+// import { Button } from 'semantic-ui-react';
 
 class Complete extends React.Component {
   constructor() {
--- react-ui/src/components/Reviewer/Student.js
+++ react-ui/src/components/Reviewer/Student.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import StudentForm from './StudentForm';
 import Mission from './Mission';
+import Complete from './Complete';
 // import Questions from './Questions';
 
 class Student extends Component {
@@ -18,6 +19,7 @@ class Student extends Component {
         <Route path="/mission/:id/casefile/:casefile_id" component={Mission} />
         <Route path="/mstestteacher/1" component={StudentForm} />
         <Route path="/form" component={StudentForm} />
+        <Route path="/mission/complete" component={Complete} />
       </div>
     );
   }
